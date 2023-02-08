d7f8bf33722d00e1c62f6216e360f7bb35914ea5
A Normal Ladd <lewisharshman1@gmail.com>
2023-02-08 13:23:26 -0700

    Upgrade to double precision
    Functions were using integer before which caused fringing on the edges of the triangle. 
    Using Line2D and graphics2D, The algorithm now uses doubles to draw the lines.

--- Gasket.java
+++ Gasket.java
@@ -1,6 +1,9 @@
 import java.util.Random;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.geom.Line2D;
 import javax.swing.JFrame;
 import javax.swing.SwingWorker;
 
@@ -47,13 +50,14 @@ public class Gasket extends JFrame {
     public void paint(Graphics graphics) //Draw here
     {
         //super.paint(graphics);
-        getContentPane().setBackground(new Color(70,80,70));
+        //getContentPane().setBackground(new Color(70,80,70));
         getGraphics().setColor(Color.BLACK);
+        Graphics2D g2 = (Graphics2D) graphics;
 
         try
         {
             triangle(10, 550, 50, graphics);
-            sTriangle(10, 980, 980, 5, graphics);
+            sTriangle(10, 980, 980, 8, g2);
         }
         catch (Exception e)
         {
@@ -82,7 +86,7 @@ public class Gasket extends JFrame {
         g.drawLine(x2, y2, x3, y3);
     }
 
-    public static void sTriangle(int x, int y, int s, int n, Graphics g)  throws Exception
+    public static void sTriangle(double x, double y, double s, int n, Graphics2D g)  throws Exception
     {
         if (n <= 0)
         {
@@ -96,19 +100,22 @@ public class Gasket extends JFrame {
         g.setColor(color);
         
         // Coordinates of three corners
-        int x1 = x; //bottom left
-        int y1 = y;
-        int x2 = (x1+s/2);  //top
-        int y2 = y1-(int)(Math.sqrt(3)*s/2);
-        int x3 = x1+s; //bottom right
-        int y3 = y1;
+        double x1 = x; //bottom left
+        double y1 = y;
+        double x2 = (x1+s/2.0);  //top
+        double y2 = y1- (Math.sqrt(3)*s/2.0);
+        double x3 = x1+s; //bottom right
+        double y3 = y1;
 
-        g.fillPolygon(new int[] {x1,x2,x3}, new int[] {y1,y2,y3}, 3);
+        //g.fillPolygon(new int[] {x1,x2,x3}, new int[] {y1,y2,y3}, 3);
+        g.draw(new Line2D.Double(x1, y1, x2, y2));
+        g.draw(new Line2D.Double(x1, y1, x3, y3));
+        g.draw(new Line2D.Double(x2, y2, x3, y3));
         
         //Offsets for triangle.
         sTriangle(x1, y1, s/2, n-1, g);
-        sTriangle((int)(x1+x2)/2, (int)(y1+y2)/2, s/2, n-1, g);
-        sTriangle((int)(x1+x3)/2, (int)(y1+y3)/2, s/2, n-1, g);
+        sTriangle((x1+x2)/2.0, (y1+y2)/2.0, s/2.0, n-1, g);
+        sTriangle((x1+x3)/2.0, (y1+y3)/2.0, s/2.0, n-1, g);
 
     }
  
